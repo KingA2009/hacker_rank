c13ca05ca6cc7020a5d3823405ef02d94e270090
KingA2009 <ajamolidinov12@gmailc.com>
2023-11-21 16:37:56 +0500

    task3 is sloved

--- task1.go
+++ task1.go
@@ -9,15 +9,6 @@
 // - int: the sum of a and b
 package main
 
-import "fmt"
-
 func solveMeFirst(a uint32, b uint32) uint32 {
 	return (a + b)
-}
-
-func main() {
-	var a, b, res uint32
-	fmt.Scanf("%v\n%v", &a, &b)
-	res = solveMeFirst(a, b)
-	fmt.Println(res)
 }
