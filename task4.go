4a2f79e32db3a468048522679479a12e9ee24d85
KingA2009 <ajamolidinov12@gmailc.com>
2023-11-22 10:26:18 +0500

    task4 is sloved

---
+++ task4.go
@@ -0,0 +1,12 @@
+//In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+package main
+
+func aVeryBigSum(ar []int64) int64 {
+	var sum int64
+	for _, a := range ar {
+		sum += a
+	}
+	return sum
+
+}
